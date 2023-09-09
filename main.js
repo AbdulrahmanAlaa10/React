23246853ecb72bd96a9954156e94448a8d8edbe4
abdulrahmanalaa10 <alaaboody65@gmail.com>
2023-09-09 21:51:32 +0300

    frist app

---
+++ main.js
@@ -0,0 +1,40 @@
+function formValidate(){
+
+
+    var username =document.getElementById("username").value;
+    var email =document.getElementById("email").value;
+    var phone =document.getElementById("phone").value;
+    var error =document.getElementById("error");
+    var text="";
+
+
+
+    if( username.length < 5){
+
+        text = " Please Enter Valid Username"; 
+        error.innerHTML = text;
+        return false;
+    }
+    
+
+    else if(email.indexOf("@") == -1 || email.length < 6) {
+        text = " Please Enter Valid Email";
+        error.innerHTML = text;
+        return false;
+    }
+    
+
+    else if (isNaN(phone) || phone.length != 11){
+
+        text = " Please Enter Valid Phone";
+        error.innerHTML = text;
+        return false;
+    }
+    
+    
+    else{
+        alert("Done");
+        return true;
+    }
+}
+
